d0e112ba4d67716cc67bd466b44b1f8ffc7737fd
abbasikov <writeumair@gmail.com>
2018-05-27 04:23:27 -0700

    small change

--- sensors/temperature/TemperatureSensor.py
+++ sensors/temperature/TemperatureSensor.py
@@ -1,4 +1,6 @@
 import time
+import random
+import requests
 class TemperatureSensor:
     serviceURL = ''
     pushDataToServer = 1
@@ -12,6 +14,11 @@ class TemperatureSensor:
 
     def pushDataToServer(self):
         while (True):
-            time.sleep(self.pushIntervalInSeconds)
-            print('TemperatureSensor pushDataToServer')
-            print('')
+            try:
+                print('TemperatureSensor pushDataToServer')
+                time.sleep(self.pushIntervalInSeconds)
+                tempInC = random.randint(10, 50)
+                response = requests.post(self.serviceURL, data={'tempInC': tempInC})
+            except Exception as inst:
+                print('Error:')
+                print(inst)
